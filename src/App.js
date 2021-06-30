4fc05103120fe155d59bbc8b2e152603cb98e75a
dimple5 <dimple@vernacular.ai>
2021-06-30 23:14:42 +0530

    updated url

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import {Row,Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 
-function App(){
+function App() {
   const [category, setCategory] = useState("");
   const [fetchedData, setFetchedData] = useState([])
   const [state, setState] = useState(false)
-
-  async function fetchData(){
+  const url = 'https://shop-n-sing.herokuapp.com/category?category='
+  async function fetchData() {
     const { data } = await axios.get(
-      'https://shop-n-fun.herokuapp.com/category?category='+ category
-      )
+      url + category
+    )
     setFetchedData(data)
   }
 
@@ -25,41 +25,41 @@ function App(){
     fetchData()
   }
 
-  return(
+  return (
     <Container className="p-3">
       <Jumbotron>
-      <Form onSubmit={handleSubmit}>
-      <Form.Label>
-        <small>Please enter the category</small></Form.Label>
-        <Row>
-          <Col><Form.Control 
-          className="w-100 p-3"
-          type="text"
-          placeholder="Category"
-          value={category}
-          onChange={e => setCategory(e.target.value)}/></Col>
-        </Row>
-      
-    </Form>
-    </Jumbotron>
+        <Form onSubmit={handleSubmit}>
+          <Form.Label>
+            <small>Please enter the category</small></Form.Label>
+          <Row>
+            <Col><Form.Control
+              className="w-100 p-3"
+              type="text"
+              placeholder="Category"
+              value={category}
+              onChange={e => setCategory(e.target.value)} /></Col>
+          </Row>
+
+        </Form>
+      </Jumbotron>
       {
-      fetchedData.map(d => <div> 
-        <br></br>
-          <Card className="card text-white bg-secondary mb-3" style={{max: '20rem'}}>
+        fetchedData.map(d => <div>
+          <br></br>
+          <Card className="card text-white bg-secondary mb-3" style={{ max: '20rem' }}>
             <Card.Title className="card-header"><h3>{d.track_name}</h3></Card.Title>
             <Card.Body>
               <Card.Text>
-              <h3><i>Artist: </i>{d.artist}</h3> 
-              <Button variant="btn btn-secondary" onClick={e=> setState(s => !s)}>Lyrics</Button>
-              {{state} ? <p>{d.lyrics}</p> : "false"}
-              
-              </Card.Text> 
+                <h3><i>Artist: </i>{d.artist}</h3>
+                <Button variant="btn btn-secondary" onClick={e => setState(s => !s)}>Lyrics</Button>
+                {{ state } ? <p>{d.lyrics}</p> : "false"}
+
+              </Card.Text>
             </Card.Body>
           </Card>
-         </div>)}
-         <code>Tracks for category: {category}</code> <input type="submit" value="Next" onClick={handleSubmit}/>
+        </div>)}
+      <code>Tracks for category: {category}</code> <input type="submit" value="Next" onClick={handleSubmit} />
     </Container>
-    
+
   )
 }
 export default App;
